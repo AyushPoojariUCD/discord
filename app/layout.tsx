801e232f9064f835a144bb462ea4b755b5083a87
AyushPoojariUCD <ayush.poojari@ucdconnect.ie>
2025-07-26 00:03:46 +0530

    feat: added clerk sign-in and sign-up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import {ClerkProvider} from '@clerk/nextjs'
+import { Open_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const font = Open_Sans({
   subsets: ["latin"],
 });
 
@@ -23,12 +18,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${font.className}`}
       >
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
